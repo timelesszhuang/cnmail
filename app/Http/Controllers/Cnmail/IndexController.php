024769ae36295f5db2c076dd3430bcd47bccdd59
yangjingzheng <1318911846@qq.com>
2017-07-18 09:57:01 +0800

    获取表的内荣

--- app/Http/Controllers/Cnmail/IndexController.php
+++ app/Http/Controllers/Cnmail/IndexController.php
@@ -14,11 +14,32 @@ class IndexController extends Controller
 {
 
     public function index()
-    {
-
+    {   //获取文件的路径
+        $path = base_path('views/num.txt');
+        //判断有没有这个文件
+        if (file_exists($path)) {
+            $contents = file_get_contents($path);
+           //判断文件夹中有没有内容
+            if (!empty($contents)) {
+                $table = explode(':', $contents);
+                $this->run($table[0], $table[1]);
+            } else {
+                //没有内容静态走模块中的方法
+                $table = Mongodb::getTableName();
+                $content = $table . ":" . "0";
+                file_put_contents($path,$content);
+                $this->run($table, 0);
+            }
+        }else{
+            //没有文件创建自动添加内容
+            $table = Mongodb::getTableName();
+            $content = $table . ":" . "0";
+            file_put_contents($path,$content);
+            $this->run($table, 0);
+        }
     }
 
-    public function run($tableName,$sendNumber)
+    public function run($tableName, $sendNumber)
     {
 
     }

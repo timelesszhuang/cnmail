a3297f9264ab579a2e548f5c52957b2638f29f6b
yangjingzheng <1318911846@qq.com>
2017-07-20 08:52:25 +0800

    获取表的内荣

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,194 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>文章详情</title>
+    <script src="../templatestatic/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript" src="../templatestatic/jquery.SuperSlide.js"></script>
+    <link href="../templatestatic/style.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<div class="top">
+    <div class="logo">企业邮箱网<span>www.qiyemail.com</span></div>
+</div>
+
+
+<div class="yuming_box">
+    <div class="ym_title"><h1>域名注册</h1></div>
+    <table>
+        <tr>
+            <td class="tb-title">域名</td>
+            <td>ghj</td>
+        </tr>
+        <tr>
+            <td class="tb-title">注册人</td>
+            <td>zcv</td>
+        </tr>
+        <tr>
+            <td class="tb-title">注册时间</td>
+            <td>bnm</td>
+        </tr>
+        <tr>
+            <td class="tb-title">过期时间</td>
+            <td>ghj</td>
+        </tr>
+        <tr>
+            <td class="tb-title">注册城市</td>
+            <td>bnm</td>
+        </tr>
+        <tr>
+            <td class="tb-title">注册邮箱</td>
+            <td>ghj</td>
+        </tr>
+        <tr>
+            <td class="tb-title">网站标题</td>
+            <td>bnm</td>
+        </tr>
+        <tr>
+            <td class="tb-title">邮箱标题</td>
+            <td>bnm</td>
+        </tr>
+    </table>
+</div>
+
+<div class="mx_box">
+    <div class="mx_title"><h1>MX记录</h1></div>
+    <table>
+        <tr class="mx-tm">
+            <td>mx</td>
+            <td>品牌</td>
+            <td>优先级</td>
+        </tr>
+        <tr>
+            <td>ghj</td>
+            <td>zcv</td>
+            <td>bnm</td>
+        </tr>
+    </table>
+</div>
+
+<form id="theform_bottom" name="theform_bottom"  >
+    <input type="hidden" name="shuaidan_type" value="1">
+    <div class="dy03_biaodan">
+        <h1>亲爱的用户，请填写以下信息参加活动</h1>
+        <div>
+            <label>公司名称：</label>
+            <input type="text" name="company" id="company" placeholder="请输入公司名称">
+            <div class="clearfix"></div>
+        </div>
+        <div>
+            <label>联系电话：</label>
+            <input type="text" name="phone" id="userphoneSales"placeholder="请输入联系电话">
+            <div class="clearfix"></div>
+        </div>
+        <div>
+            <label>联系人：</label>
+            <input type="text"name="name" id="userNameSales"placeholder="请输入联系人">
+            <div class="clearfix"></div>
+        </div>
+        <div>
+            <label>联系邮箱：</label>
+            <input type="text"name="email" id="email"placeholder="请输入联系邮箱">
+            <input type="hidden"  value="<?php echo $ip; ?>" name="ip"/>
+            <input type="hidden"  value="<?php echo $query_string; ?>" name="query_string"/>
+            <!--这个参数是从搜索引擎中来-->
+            <input type="hidden" value="<?php echo $key_word; ?>" name="key_word"/>
+            <!--搜索引擎-->
+            <input type="hidden" value="<?php echo $search_engine; ?>" name="search_engine"/>
+            <!--搜索引擎传递过来的地域信息-->
+            <input type="hidden" value="<?php echo $s_val; ?>" name="s_val"/>
+            <!--位置信息 比如是qiangbi  还是胜途的区分-->
+            <input type="hidden" value="<?php echo $pos; ?>" name="pos"/>
+            <!--表示是谁的客户 表示salesmen 中的 职员的id-->
+            <input type="hidden" value="<?php echo $s?>" name="s">
+            <div class="clearfix"></div>
+        </div>
+        <input class="btn" name="submit_bottom" type="button" id="submit_bottom" value="马上参加">
+    </div>
+</form>
+<script type="text/javascript">
+    $(function(){
+        $('#submit_bottom').click(function () {
+            var data = $('#theform_bottom').serialize();
+            var url = 'http://salesman.cc/index.php/Shuaidan_ceshi/PublicTry/index';
+            var name=$("#userNameSales").val();
+            var phone=$("#userphoneSales").val();
+            var email=$("#email").val();
+            var company=$("#company").val();
+            $("#theform_bottom")[0].reset();
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                //因为如果是异步执行的话  没有返回就执行return 了 但是同步的话还是会有问题的 比如长时间没有相应的话会阻塞
+                async: true,
+                url: url,
+                data: data,
+                success: function (data) {
+                    if(data.status==20){
+                        $("#userNameSales").val(name);
+                        $("#userphoneSales").val(phone);
+                        $("#email").val(email);
+                        $("#company").val(company);
+                    }
+                    alert(data.msg);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert('尊敬的用户，我们已经收到您的请求，稍后会有专属客服为您服务。');
+                }
+            });
+        });
+    })
+</script>
+
+
+<div class="pptj">
+    <div class="wz_title">
+        <a href="">
+            <span class="ppw">邮箱品牌推荐</span><span class="gd">更多 &nbsp;>>&nbsp;</span><div class="clearfix"></div>
+        </a>
+    </div>
+    <ul>
+        <li><a href=""><img src="../templatestatic/l1.jpg"></a></li>
+        <li><a href=""><img src="../templatestatic/l2.jpg" /></a></li>
+        <li><a href=""><img src="../templatestatic/l3.jpg" /></a></li>
+        <li><a href=""><img src="../templatestatic/l4.jpg" /></a></li>
+        <li><a href=""><img src="../templatestatic/l5.jpg" /></a></li>
+        <li><a href=""><img src="../templatestatic/l6.jpg" /></a></li>
+        <li><a href=""><img src="../templatestatic/l7.jpg" /></a></li>
+        <li><a href=""><img src="../templatestatic/l8.jpg" /></a></li>
+        <li><a href=""><img src="../templatestatic/l7.jpg" /></a></li>
+        <li><a href=""><img src="../templatestatic/l8.jpg" /></a></li>
+        <div class="clearfix"></div>
+    </ul>
+</div>
+
+<div class="bottom">
+    <div class="end">
+        <div class="el">
+            <div class="dxx">
+                全国企业邮箱服务电话：4006-360-163<br/>
+                © 2004-2012 北京强比科技有限公司&nbsp;&nbsp;&nbsp;&nbsp; 济南强比科技有限公司&nbsp;&nbsp;&nbsp;&nbsp; 中国企业邮箱网
+            </div>
+        </div>
+        <div class="er"><img src="../templatestatic/er.jpg" /></div>
+    </div>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,3 +1,158 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>文章列表</title>
+    <script src="templatestatic/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript" src="templatestatic/jquery.SuperSlide.js"></script>
+    <link href="templatestatic/style.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<div class="top_box">
+    <div class="top">
+        <div class="logo">企业邮箱网<span>www.qiyemail.com</span></div>
+    </div>
+</div>
+
+
+<div class="yxwz">
+    <div class="wz_title">
+        <a href="">
+            <span class="ppw">邮箱文章</span><span class="gd">更多 &nbsp;>>&nbsp;</span><div class="clearfix"></div>
+        </a>
+    </div>
+    <div class="wz_box">
+        <ul class="wz">
+            <li><a href="">1.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">2.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">3.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">4.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">5.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">6.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">7.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">8.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">9.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">10.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">11.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">12.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">13.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">14.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">15.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">16.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">17.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">18.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">19.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">20.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">21.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">22.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">23.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">24.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">25.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">26.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">27.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">28.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">29.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">30.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">31.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">32.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">33.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">34.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">35.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">36.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">37.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">38.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">39.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">40.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">41.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">42.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">43.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">44.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">45.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">46.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">47.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">48.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">49.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+            <li><a href="">50.谁才是你文件存储的最佳选择?</a><span class="sj">2016-12-29</span></li>
+        </ul>
+        <ul class="pagination">
+            <li class="disabled"><span>«</span></li>
+            <li class="active"><span>1</span></li>
+            <li><a href="/questionlist/3.html?page=2">2</a></li>
+            <li><a href="/questionlist/3.html?page=2">»</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="pptj">
+    <div class="wz_title">
+        <a href="">
+            <span class="ppw">邮箱品牌推荐</span><span class="gd">更多 &nbsp;>>&nbsp;</span><div class="clearfix"></div>
+        </a>
+    </div>
+    <ul>
+        <li><a href=""><img src="templatestatic/l1.jpg"></a></li>
+        <li><a href=""><img src="templatestatic/l2.jpg" /></a></li>
+        <li><a href=""><img src="templatestatic/l3.jpg" /></a></li>
+        <li><a href=""><img src="templatestatic/l4.jpg" /></a></li>
+        <li><a href=""><img src="templatestatic/l5.jpg" /></a></li>
+        <li><a href=""><img src="templatestatic/l6.jpg" /></a></li>
+        <li><a href=""><img src="templatestatic/l7.jpg" /></a></li>
+        <li><a href=""><img src="templatestatic/l8.jpg" /></a></li>
+        <li><a href=""><img src="templatestatic/l7.jpg" /></a></li>
+        <li><a href=""><img src="templatestatic/l8.jpg" /></a></li>
+        <div class="clearfix"></div>
+    </ul>
+</div>
+
+<div class="youqing">
+    <div class="wz_title">
+        <a href="">
+            <span class="ppw">友情链接</span><span class="gd">更多 &nbsp;>>&nbsp;</span>
+        </a>
+    </div>
+    <ul>
+        <li><a href="">怎样申请企业邮箱</a></li>
+        <li><a href="">河南企业邮箱</a></li>
+        <li><a href="">网易企业邮箱</a></li>
+        <li><a href="">163企业邮箱</a></li>
+        <li><a href="">淄博企业邮箱</a></li>
+        <li><a href="">威海企业邮箱</a></li>
+        <li><a href="">山东企业邮箱</a></li>
+        <li><a href="">怎样申请企业邮箱</a></li>
+        <li><a href="">河南企业邮箱</a></li>
+        <li><a href="">网易企业邮箱</a></li>
+        <li><a href="">163企业邮箱</a></li>
+        <li><a href="">淄博企业邮箱</a></li>
+        <li><a href="">威海企业邮箱</a></li>
+        <li><a href="" class="yq_last">山东企业邮箱</a></li>
+    </ul>
+</div>
+<div class="bottom">
+    <div class="end">
+        <div class="el">
+            <div class="dxx">
+                全国企业邮箱服务电话：4006-360-163<br/>
+                © 2004-2012 北京强比科技有限公司&nbsp;&nbsp;&nbsp;&nbsp; 济南强比科技有限公司&nbsp;&nbsp;&nbsp;&nbsp; 中国企业邮箱网
+            </div>
+        </div>
+        <div class="er"><img src="templatestatic/er.jpg" /></div>
+    </div>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,8 +167,17 @@
 
 </div>
 <div>
-    <p><a href="{{$pre_page}}">上一页</a></p>
-    <p><a href="{{$next_page}}">下一页</a></p>
+
+    <p>
+        @if(isset($pre_page))
+            <a href="{{$pre_page}}">上一页</a>
+        @endif
+    </p>
+    <p>
+        @if(isset($next_page))
+        <a href="{{$next_page}}">下一页</a>
+        @endif
+    </p>
 </div>
 </body>
 
